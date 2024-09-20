20f233cb17c21c4eb2455a46142afbe058c66b68
Satyam Kumar Choudhary <choudharysatyam951478@gmail.com>
2024-09-20 09:13:06 +0530

    Changes

--- Saumya_Classes/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-class HelloWorld
-{
-    public static void main(String args[])
-    {
-        System.out.println("Hello World");
-        System.out.print("Hello");
-        System.out.print("HI");
-    }
-}
